95454dc81f0069a7132bc25f6f866b495d40aca3
Cazaks <calebezak@gmail.com>
2025-07-10 14:51:40 +0100

    just etrac touching

--- StudentGrade1.java
+++ StudentGrade1.java
@@ -5,9 +5,9 @@ public class StudentGrade1 {
 		Scanner scan = new Scanner(System.in);
 		
 		int total = 0;
-		int counter = 1;
+		int counter = 0;
 
-		while(counter <= 10){
+		/*while(counter <= 10){
 		System.out.print("Enter student grade (0-100): ");
 		int grade = scan.nextInt();
 		total = total + grade;
@@ -17,6 +17,35 @@ public class StudentGrade1 {
 	int average = total / 10;
 
 	System.out.printf("%nTotal of 10 grade is: %d%n", total);
-	System.out.printf("The average of the class is: %d%n", average);				
+	System.out.printf("The average of the class is: %d%n", average);*/
+
+	
+		System.out.print("Enter student grade (0-100), enter - 1 to quit: ");
+		int grade = scan.nextInt();
+		
+		while(grade != -1){
+		if(grade >= 0 && grade <= 100){
+		total += grade;
+		counter += 1;
+		}
+		else{
+		System.out.println("imvalid grade. Enter grade between 0 and 100");
+		}
+
+		System.out.print("Enter student grade (0-100): ");
+		grade = scan.nextInt();
+		}
+
+		if(counter != 0){
+		double average = (double) total / counter;
+		
+		System.out.printf("Total of %dn grade is: %d%n", counter, total);
+		System.out.printf("The average of student score is: %.2f%n", average);
+		}
+		else{
+		System.out.println("No grade was entered");
+		}
+					
 	}	
 }
+
