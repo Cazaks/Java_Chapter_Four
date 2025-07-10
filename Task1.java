fbaa480f74a88dbb0ad7aa90c07b4b5666af1dfa
Cazaks <calebezak@gmail.com>
2025-07-10 08:30:03 +0100

    Users deciding what to print

--- Task1.java
+++ Task1.java
@@ -2,10 +2,14 @@ import java.util.Scanner;
 public class Task1 {
 	public static void main(String[] args){
 Scanner scan = new Scanner(System.in);
+
+		System.out.println("What message would you like me to print? ");
+		String message = scan.nextLine();
+
 		System.out.print("How many times should I print the message? ");
 		int times = scan.nextInt();
 		for(int i = 1; i <= times; i++){
-		System.out.println(i + ". I am learning Java");
+		System.out.println(i + ". " + message);
 		}			
 	}	
 }
