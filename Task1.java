362d7ac8949c13a1d1088141f048f36a799bb69a
Cazaks <calebezak@gmail.com>
2025-07-10 08:19:47 +0100

    The using imputing the number of time he want the message to be printed

--- Task1.java
+++ Task1.java
@@ -1,9 +1,11 @@
-
+import java.util.Scanner;
 public class Task1 {
 	public static void main(String[] args){
-
-		for(int i = 1; i <= 5; i++){
+Scanner scan = new Scanner(System.in);
+		System.out.print("How many times should I print the message? ");
+		int times = scan.nextInt();
+		for(int i = 1; i <= times; i++){
 		System.out.println(i + ". I am learning Java");
-		}				
+		}			
 	}	
 }
