1f109eacfd4a7e6855a0d5c283d7e951f6820736
Cazaks <calebezak@gmail.com>
2025-07-15 04:54:41 +0100

    Printing numbers exercise

---
+++ PrintingNumbersReverse.java
@@ -0,0 +1,15 @@
+public class PrintingNumbersReverse{
+	public static void main(String[]args){
+
+		//for(int number = 100; number > 0; number--){
+		//System.out.print(number + " ");
+		//}
+
+		int number = 100;
+		while(number > 0){
+		System.out.print(number + " ");
+		number--;
+		}
+}
+
+}
